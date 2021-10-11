c7c3fa37a629deeb6d786e3fb2e7a79d1d428b4e
Alice Sales <programmeralice@gmail.com>
2021-10-11 16:37:58 -0300

    challenge solved

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,31 @@
-export function Content() {
-  // Complete aqui
+import { Header } from './Header';
+import { MovieCard } from './MovieCard';
+
+interface ContentProps {
+  category: string;
+  movies: {
+      imdbID: string;
+      Title: string;
+      Poster: string;
+      Ratings: Array<{
+        Source: string;
+        Value: string;
+      }>;
+      Runtime: string;
+  }[];
+}
+
+export function Content(props: ContentProps) {
+  return (
+    <div className="container">
+      <main>
+        <Header category={props.category}/>
+        <div className="movies-list">
+          {props.movies.map(movie => (
+            <MovieCard key ={movie.imdbID} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
+          ))}
+        </div>
+      </main>
+    </div>
+  );
 }
